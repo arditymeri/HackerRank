3de13211bcbf0eea9b8b90cbd05dc389f5759804
Ardit Ymeri <ditiymeri@gmail.com>
2021-04-25 18:25:32 +0200

    ADD Circular Array Rotation - improvements and docs

--- src/fourthstar/CircularArrayRotation.java
+++ src/fourthstar/CircularArrayRotation.java
@@ -5,8 +5,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * John Watson knows of an operation called a right circular rotation on an array of integers.
+ * One rotation operation moves the last array element to the first position and shifts all remaining elements
+ * right one. To test Sherlock's abilities, Watson provides Sherlock with an array of integers.
+ * Sherlock is to perform the rotation operation a number of times then determine the value of
+ * the element at a given position.
+ * <p>
+ * For each array, perform a number of right circular rotations and return the values of the elements at
+ * the given indices.
+ */
 public class CircularArrayRotation {
 
+    /**
+     *
+     * @param a the array to rotate
+     * @param k the rotation count
+     * @param queries the indices to report
+     * @return the values in the rotated {@code a} as requested in {@code queries}.
+     */
     public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
         int length = a.size();
         int offset = k % length;
@@ -14,7 +31,7 @@ public class CircularArrayRotation {
         for (Integer q : queries) {
             int index = q - offset;
             if (index < 0) {
-                index = length - 1 - (offset - (q + 1));
+                index = length - offset + q;
             }
             newValues.add(a.get(index));
         }
@@ -22,10 +39,11 @@ public class CircularArrayRotation {
     }
 
     public static void main(String[] args) {
-//        List<Integer> a = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-        List<Integer> a = Arrays.asList(1, 2, 3);
-        int k = 2;
-        List<Integer> q = Arrays.asList(0, 1, 2);
+        List<Integer> a = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+//        List<Integer> a = Arrays.asList(1, 2, 3);
+        int k = 9;
+        List<Integer> q = Arrays.asList(0, 1, 2, 3, 4, 5, 6);
+//        List<Integer> q = Arrays.asList(0, 1, 2);
         List<Integer> shifted = circularArrayRotation(a, k, q);
         String newValues = shifted.stream().map(Object::toString).collect(Collectors.joining(","));
         System.out.println(newValues);
