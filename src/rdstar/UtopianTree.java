b43a4390da9fb9f42eeda74f247da8a6ae7efc84
Ardit Ymeri <ditiymeri@gmail.com>
2021-04-25 13:37:46 +0200

    ADD Utopian tree - functional style ;)

--- src/rdstar/UtopianTree.java
+++ src/rdstar/UtopianTree.java
@@ -1,21 +1,19 @@
 package rdstar;
 
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class UtopianTree {
 
     public static int utopianTree(int n) {
         Function<Integer, Integer> inc = i -> i + 1;
         Function<Integer, Integer> dup = i -> i * 2;
-        int height = 1;
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 1) {
-                height = dup.apply(height);
-            } else {
-                height = inc.apply(height);
-            }
-        }
-        return height;
+
+        return Stream.iterate(1, i -> i + 1)
+                .limit(n)
+                .map(i -> {return i%2 == 1 ? dup:inc;})
+                .reduce(Function.identity(), Function::andThen)
+                .apply(1);
     }
 
     public static void main(String[] args) {
