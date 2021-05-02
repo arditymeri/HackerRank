87fe4b25c0ba2d98ce2b1b0e2b2630a29bed16d7
Ardit Ymeri <ditiymeri@gmail.com>
2021-05-02 13:31:40 +0200

    ADD Non Divisible Subset - discarding elements
    Timout exception

--- src/fourthstar/NonDivisibleSubset.java
+++ src/fourthstar/NonDivisibleSubset.java
@@ -13,6 +13,43 @@ import static java.util.stream.Collectors.toList;
 public class NonDivisibleSubset {
 
     public static int nonDivisibleSubset(int k, List<Integer> s) {
+        while (true) {
+            int index = findMaxCol(s, k);
+            if (index < 0) {
+                break;
+            }
+            s.remove(index);
+        }
+        return s.size();
+    }
+
+    private static int findMaxCol(List<Integer> s, int k) {
+        int index = -1;
+        int max = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int c = 0;
+            int n = s.get(i);
+            for (int j = 0; j < s.size(); j++) {
+                if (i != j) {
+                    int m = s.get(j);
+                    if((m+n)%k == 0) {
+                        c++;
+                    }
+                }
+            }
+            if (c > max) {
+                index = i;
+                max = c;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * Cross matrix solution
+     */
+
+    public static int nonDivisibleSubset2(int k, List<Integer> s) {
         int removed = 0;
         int n = s.size();
         short[][] cross = calcCrossMatrix(k, s);
@@ -72,7 +109,7 @@ public class NonDivisibleSubset {
     public static void main(String[] args) {
         long start = System.nanoTime();
 //        int max = nonDivisibleSubset(3, Arrays.asList(1, 7, 2, 4));
-        int max = nonDivisibleSubset(7, Arrays.asList(278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436));
+        int max = nonDivisibleSubset(7, new ArrayList<>(Arrays.asList(278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436)));
         System.out.println(max);
         Duration duration = Duration.ofNanos(System.nanoTime() - start);
         System.out.println("Duration:" + duration.toSeconds());
