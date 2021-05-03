ac32e7d49d449b3ad0ee2b518cbe4fd73eda15c1
Ardit Ymeri <ditiymeri@gmail.com>
2021-05-03 20:02:34 +0200

    ADD Non Divisible Subset - remove unused code

--- src/fourthstar/NonDivisibleSubset.java
+++ src/fourthstar/NonDivisibleSubset.java
@@ -36,103 +36,6 @@ public class NonDivisibleSubset {
         return originalN - toRemove;
     }
 
-    /*
-     * Removing elements. Timeout exception.
-     * Find the number with most hits. Remove it. Until there are no more numbers with hits.
-     */
-    public static int nonDivisibleSubset3(int k, List<Integer> s) {
-        while (true) {
-            int index = findMaxCol(s, k);
-            if (index < 0) {
-                break;
-            }
-            s.remove(index);
-        }
-        return s.size();
-    }
-
-    private static int findMaxCol(List<Integer> s, int k) {
-        int index = -1;
-        int max = 0;
-        for (int i = 0; i < s.size(); i++) {
-            int c = 0;
-            int n = s.get(i);
-            for (int j = 0; j < s.size(); j++) {
-                if (i != j) {
-                    int m = s.get(j);
-                    if((m+n)%k == 0) {
-                        c++;
-                    }
-                }
-            }
-            if (c > max) {
-                index = i;
-                max = c;
-            }
-        }
-        return index;
-    }
-
-    /**
-     * Cross matrix solution
-     */
-
-    public static int nonDivisibleSubset2(int k, List<Integer> s) {
-        int removed = 0;
-        int n = s.size();
-        short[][] cross = calcCrossMatrix(k, s);
-        while (hasOnes(cross, n)) {
-            int index = findMaxCol(cross, n);
-            for (int i = 0; i < n; i++) {
-                cross[i][index] = 0;
-                cross[index][i] = 0;
-            }
-            removed++;
-        }
-        return n - removed;
-    }
-
-    private static int findMaxCol(short[][] cross, int n) {
-        int index = 0;
-        int maxSum = 0;
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = 0; j < n; j++) {
-                sum += cross[i][j];
-            }
-            if (sum > maxSum) {
-                index = i;
-                maxSum = sum;
-            }
-        }
-        return index;
-    }
-
-    private static boolean hasOnes(short[][] cross, int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (cross[i][j] == 1) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private static short[][] calcCrossMatrix(int k, List<Integer> s) {
-        int size = s.size();
-        short[][] crossMatrix = new short[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                boolean divisible = false;
-                if (i != j) {
-                    divisible = (s.get(i) + s.get(j)) % k == 0;
-                }
-                crossMatrix[i][j] = divisible ? (short) 1 : (short)0;
-            }
-        }
-        return crossMatrix;
-    }
 
     public static void main(String[] args) {
         long start = System.nanoTime();
@@ -142,31 +45,6 @@ public class NonDivisibleSubset {
         Duration duration = Duration.ofNanos(System.nanoTime() - start);
         System.out.println("Duration:" + duration.toSeconds());
 
-    }
-
-    public static int findNextMaxSubset(int k, List<Integer> subset, List<Integer> set) {
-        int maxSize = subset.size();
-        for (Integer i : set) {
-            boolean nonDivisible = true;
-            for (Integer j : subset) {
-                if ((i + j) % k == 0) {
-                    nonDivisible = false;
-                    break;
-                }
-            }
-            if (nonDivisible) {
-                maxSize = subset.size() + 1;
-                List<Integer> newSubset = new ArrayList<>(subset);
-                newSubset.add(i);
-                List<Integer> newSet = new ArrayList<>(set);
-                newSet.remove(i);
-                int newMax = findNextMaxSubset(k, newSubset, newSet);
-                if (newMax > maxSize) {
-                    maxSize = newMax;
-                }
-            }
-        }
-        return maxSize;
     }
 }
 
