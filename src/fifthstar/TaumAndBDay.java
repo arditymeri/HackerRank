4bca3cba6fc03e9a9a4b3ed8ae122ed5ffbaee4f
Ardit Ymeri <ditiymeri@gmail.com>
2021-05-24 22:06:43 +0200

    ADD Taum and BDay - docs

--- src/fifthstar/TaumAndBDay.java
+++ src/fifthstar/TaumAndBDay.java
@@ -1,5 +1,15 @@
 package fifthstar;
 
+/**
+ * Taum is planning to celebrate the birthday of his friend, Diksha.
+ * There are two types of gifts that Diksha wants from Taum: one is black and the other is white.
+ * To make her happy, Taum has to buy b black gifts and w white gifts.
+ *
+ * The cost of each black gift is bc units.
+ * The cost of every white gift is wc units.
+ * The cost to convert a black gift into white gift or vice versa is z units.
+ * Determine the minimum cost of Diksha's gifts.
+ */
 public class TaumAndBDay {
 
     public static long taumBday(int b, int w, int bc, int wc, int z) {
